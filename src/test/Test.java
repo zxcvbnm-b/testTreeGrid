95528e141ab6af66b5119c180180a5e8a01765e7
wuzhan <wuzhan@asiainfo.com>
2019-09-03 14:59:32 +0800

    创建layui使用demo

---
+++ src/test/Test.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i=1000;i<=3000;i++){
+            System.out.println("{\"id\":\""+i+"\", \"pId\":\"111\", \"name\":\"大西瓜["+i+"]\"},");
+        }
+    }
+}
